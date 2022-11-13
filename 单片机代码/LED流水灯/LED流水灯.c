765ad5c85997430d19a49070954ccd4f1e3f6199
Lightningyc <2034829801@qq.com>
2022-11-13 22:38:26 +0800

    tmd

---
+++ 单片机代码/LED流水灯/LED流水灯.c
@@ -0,0 +1,43 @@
+#include <REGX52.H>
+#include <INTRINS.H>//Ïû³ýÑÓ³Ù
+void Delay500ms()		//@12.000MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();//ÓÐÒ»Î¢ÃëµÄÑÓ³Ù
+	i = 4;
+	j = 205;
+	k = 187;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
+
+void main()
+{
+while(1)
+ {
+	 P2=0xFE;//1111 1110
+	Delay500ms();
+	 P2=0xFD;//1111 1101
+	Delay500ms();
+	 P2=0xFB;//1111 1011
+	Delay500ms();
+	 P2=0xF7;//1111 0111
+	Delay500ms();
+	 P2=0xEF;//1110 1111
+	Delay500ms();
+	 P2=0xDF;//1101 1111
+	Delay500ms();
+	 P2=0xBF;//1011 1111
+	Delay500ms();
+	 P2=0x7F;//0111 1111
+	Delay500ms(); 
+ }
+}
+	
